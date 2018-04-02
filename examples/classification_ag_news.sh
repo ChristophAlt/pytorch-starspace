3170bc499d40143caea0237cbccc6d61e18d6f5f
Christoph Alt <christoph.alt@dfki.de>
2018-04-02 19:41:53 +0200

    added missing example

---
+++ examples/classification_ag_news.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+DATASET=ag_news
+
+MODELDIR=/tmp/starspace/models
+DATADIR=/tmp/starspace/data
+
+mkdir -p "${MODELDIR}"
+mkdir -p "${DATADIR}"
+
+
+echo "Downloading AG news dataset"
+if [ ! -f "${DATADIR}/${DATASET}_csv/train.csv" ]
+then
+    wget -c "https://s3.amazonaws.com/fair-data/starspace/ag_news_csv.tar.gz" -O "${DATADIR}/${DATASET[0]}_csv.tar.gz"
+    tar -xzvf "${DATADIR}/${DATASET}_csv.tar.gz" -C "${DATADIR}"
+  fi
+
+
+echo "Start training on AG news dataset"
+./starspace train \
+  --train-file "${DATADIR}/${DATASET}_csv/train.csv" \
+  --model-path "${MODELDIR}/${DATASET}" \
+  --dataset_format ${DATASET} \
+  --d_embed=10 \
+  --lr=0.01 \
+  --epochs=5
+
+
+echo "Evaluate trained model"
+./starspace test \
+  --test-file "${DATADIR}/${DATASET}_csv/test.csv" \
+  --model-path "${MODELDIR}/${DATASET}" \
+  --dataset_format ${DATASET}
